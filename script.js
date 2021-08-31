4d528aae3b8a0fb05c2249a2d6b28057e47891ed
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2021-08-30 19:09:53 -0700

    Add html markup and selects DOM elements

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
